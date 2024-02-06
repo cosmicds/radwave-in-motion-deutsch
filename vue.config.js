70ba08a50c36cd05ebbe7b99e6f93007a417f062
Carifio24 <carifio24@gmail.com>
2024-02-06 13:39:06 -0500

    Fix typo in Vue configuration.

--- vue.config.js
+++ vue.config.js
@@ -9,7 +9,7 @@ module.exports = defineConfig({
   configureWebpack: {
     plugins: [
       new VuetifyPlugin(),
-      webpack.DefinePlugin({
+      new webpack.DefinePlugin({
         // https://github.com/vuejs/vue-cli/pull/7443
         __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false',
       })
